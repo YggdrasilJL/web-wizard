b9441677d263de3d874cadd8319f7f91a396189b
YggdrasilJL <yggdrasiljl@gmail.com>
2023-09-19 10:23:16 -0300

    initial file structure and setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">hello</h1>
+    </div>
+  );
+}
+
+export default App;
