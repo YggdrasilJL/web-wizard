0f55ed0a5e119c6dd6386f34e2648f96650fb0f1
YggdrasilJL <yggdrasiljl@gmail.com>
2023-09-30 18:14:10 -0300

    add more projects to section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -45,8 +45,24 @@ const Project = () => {
       id: 1,
       name: 'Mapbrew Haven',
       desc: 'Mapbrew Haven is your go-to tool for tabletop RPG maps. Craft and download your own maps to add a personalized touch to your sessions!',
-      img: 'src/assets/mapbrew.png',
+      img: 'src/assets/portfolio/mapbrew.png',
       link: 'https://mapbrew-websitee-20a575919a4b.herokuapp.com/login',
+      repo: 'https://github.com/YggdrasilJL/mapbrew-haven',
+    },
+    {
+      id: 2,
+      name: 'Cosmic Cocktails',
+      desc: 'Cosmic Cocktails is a unique platform that mixes astrology with mixology. Discover a surprise cocktail tailored to your zodiac sign with each click!',
+      img: 'src/assets/portfolio/cosmic-cocktails.png',
+      link: 'https://graycoded.github.io/cosmic-cocktail-2.0',
+      repo: 'https://github.com/YggdrasilJL/cosmic-cocktails',
+    },
+    {
+      id: 3,
+      name: 'SVG Creator',
+      desc: 'A simple and straightforward tool that empowers you to effortlessly create custom SVG images from the command line using inquirer.',
+      img: 'src/assets/portfolio/svg-creator.png',
+      link: 'https://github.com/YggdrasilJL/svgCreator-NODEJS',
     },
   ];
 
@@ -54,18 +70,30 @@ const Project = () => {
     <div className="text-white">
       <div className="flex flex-col items-center gap-y-10 mt-10 min-h-screen">
         <h1 className="text-5xl">Projects:</h1>
-        <div>
-          {projectList.map(({ id, name, desc, img, link }) => (
-            <div key={id}>
+        <div className="flex flex-col gap-4">
+          {projectList.map(({ id, name, desc, img, link, repo }) => (
+            <div
+              key={id}
+              className=" w-auto flex p-4 rounded-xl border hover:scale-105 hover:shadow-xl hover:shadow-opacityMagenta hover:bg-opacityMagenta  duration-700"
+            >
               <a href={link} target="_blank">
-                <div className="flex w-fit p-4 rounded-xl border hover:scale-105 hover:shadow-xl hover:shadow-opacityMagenta hover:bg-opacityMagenta  duration-700">
-                  <img src={img} width={200} className="rounded-lg" />
+                <div className="flex">
+                  <img
+                    src={img}
+                    width={200}
+                    className="rounded-lg object-contain"
+                  />
                   <div className="p-3">
                     <h3 className="text-2xl mb-2">{name}</h3>
                     <p className="max-w-md text-gray-400">{desc}</p>
                   </div>
                 </div>
               </a>
+              {repo ? (
+                <a href={repo} className="text-magenta border border-magenta w-fit h-fit p-2 rounded-lg">
+                  <p>Repository</p>
+                </a>
+              ) : null}
             </div>
           ))}
         </div>
@@ -73,7 +101,7 @@ const Project = () => {
           <h2 className="text-4xl text-center mb-8">
             Technologies I've worked with:
           </h2>
-          <div className="flex gap-5">
+          <div className="flex gap-5 mb-20">
             {techList.map(({ id, name, src, alt }) => (
               <img
                 className="object-contain"
