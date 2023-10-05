78c1cc8bfe49c1c7681b8058b44ec63fbe055177
YggdrasilJL <yggdrasiljl@gmail.com>
2023-10-05 13:27:58 -0300

    add server for heroku

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Serve static assets (your built React app)
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Define your API routes or other server logic here
+
+// Send the React app for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
