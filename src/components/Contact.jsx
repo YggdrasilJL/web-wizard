58dca643f758981744342d77ce7d1fe815a77340
YggdrasilJL <yggdrasiljl@gmail.com>
2023-10-13 20:57:11 -0300

    add form validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,22 +7,65 @@ const Contact = () => {
     message: '',
   });
 
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
     });
-  };
 
-  const [errorVisible, setErrorVisible] = useState(false);
-
-  const toggleErr = () => {
-    setErrorVisible(!errorVisible);
+    // Clear the corresponding validation error when the user starts typing.
+    setErrors({
+      ...errors,
+      [name]: '',
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Perform form validation
+    let valid = true;
+    const newErrors = { name: '', email: '', message: '' };
+
+    if (formData.name.trim() === '') {
+      newErrors.name = 'Name is required';
+      valid = false;
+    }
+
+    if (formData.email.trim() === '') {
+      newErrors.email = 'Email address is required';
+      valid = false;
+    } else if (!isValidEmail(formData.email)) {
+      newErrors.email = 'Invalid email address';
+      valid = false;
+    }
+
+    if (formData.message.trim() === '') {
+      newErrors.message = 'Message is required';
+      valid = false;
+    }
+
+    if (valid) {
+      // The form is valid, you can proceed with submission or other actions here.
+      // For example, you can submit the form data to your backend.
+      console.log('Form is valid, submitting data:', formData);
+    } else {
+      // There are validation errors, update the state to display errors.
+      setErrors(newErrors);
+    }
+  };
+
+  const isValidEmail = (email) => {
+    // Simple email validation using a regular expression.
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   };
 
   return (
@@ -38,7 +81,7 @@ const Contact = () => {
           method="POST"
           id="form"
           noValidate
-          // onSubmit={handleSubmit}
+          onSubmit={handleSubmit}
         >
           <div className="relative z-0 w-full mb-5">
             <input
@@ -46,8 +89,9 @@ const Contact = () => {
               name="name"
               placeholder=" "
               required
-              className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none
-               focus:outline-none focus:ring-0 focus:border-aqua border-gray-200"
+              className={`pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-aqua border-gray-200 ${
+                errors.name ? 'border-red-500' : ''
+              }`}
               value={formData.name}
               onChange={handleInputChange}
             />
@@ -57,9 +101,11 @@ const Contact = () => {
             >
               Name
             </label>
-            <span className="text-sm text-red-600 hidden" id="error">
-              Name is required
-            </span>
+            {errors.name && (
+              <span className="text-sm text-red-600" id="error">
+                {errors.name}
+              </span>
+            )}
           </div>
 
           <div className="relative z-0 w-full mb-5">
@@ -68,8 +114,9 @@ const Contact = () => {
               name="email"
               placeholder=" "
               required
-              className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none
-               focus:outline-none focus:ring-0 focus:border-aqua border-gray-200"
+              className={`pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-aqua border-gray-200 ${
+                errors.email ? 'border-red-500' : ''
+              }`}
               value={formData.email}
               onChange={handleInputChange}
             />
@@ -79,9 +126,11 @@ const Contact = () => {
             >
               Email
             </label>
-            <span className="text-sm text-red-600 hidden" id="error">
-              Email address is required
-            </span>
+            {errors.email && (
+              <span className="text-sm text-red-600" id="error">
+                {errors.email}
+              </span>
+            )}
           </div>
 
           <div className="relative z-0 w-full mb-5">
@@ -90,8 +139,9 @@ const Contact = () => {
               placeholder=" "
               required
               rows={5}
-              className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none
-               focus:outline-none focus:ring-0 focus:border-aqua border-gray-200"
+              className={`pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-aqua border-gray-200 ${
+                errors.message ? 'border-red-500' : ''
+              }`}
               value={formData.message}
               onChange={handleInputChange}
             />
@@ -101,14 +151,15 @@ const Contact = () => {
             >
               Message
             </label>
-            <span className="text-sm text-red-600 hidden" id="error">
-              Message is required
-            </span>
+            {errors.message && (
+              <span className="text-sm text-red-600" id="error">
+                {errors.message}
+              </span>
+            )}
           </div>
 
           <button
-            className="w-full px-6 py-3 mt-3 text-lg text-white transition-all duration-300 ease-linear rounded-lg
-             shadow outline-none bg-darkPurple hover:outline-1 hover:outline-lightAqua hover:bg-magenta hover:scale-105 hover:shadow-lg focus:outline-none"
+            className="w-full px-6 py-3 mt-3 text-lg text-white transition-all duration-300 ease-linear rounded-lg shadow outline-none bg-darkPurple hover:outline-1 hover:outline-lightAqua hover:bg-magenta hover:scale-105 hover:shadow-lg focus:outline-none"
             type="submit"
           >
             Submit
