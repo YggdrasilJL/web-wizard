dfd6529400efa22b834696530c33943791ff7eac
YggdrasilJL <yggdrasiljl@gmail.com>
2025-05-29 00:00:20 -0300

    vite base changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/web-wizard/',
+  base: 'https://yggdrasiljl.github.io/web-wizard/',
   plugins: [react()],
 });
