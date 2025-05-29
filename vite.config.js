a62de65a45c068e55fe430206292f186364104eb
YggdrasilJL <yggdrasiljl@gmail.com>
2025-05-29 00:10:23 -0300

    vite base changed AGAIN

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://yggdrasiljl.github.io/web-wizard/',
+  base: '/web-wizard/',
   plugins: [react()],
 });
